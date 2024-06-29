eb66497ef318f7effb3b87b6a6add4489f6ff4a7
Trần Việt Anh <tranvietanhcbh>
2024-06-29 22:34:31 +0700

    feat: sua o input

--- js/product.js
+++ js/product.js
@@ -1,13 +1,15 @@
 // Chọn phần tử ảnh lớn
 const bigImg = document.querySelector(".product-content-left-big-img img");
 // Chọn tất cả các phần tử ảnh nhỏ
-const smallImgs = document.querySelectorAll(".product-content-left-small-img img");
+const smallImgs = document.querySelectorAll(
+  ".product-content-left-small-img img"
+);
 
 // Thêm sự kiện click cho từng ảnh nhỏ để thay đổi ảnh lớn
-smallImgs.forEach(function(imgItem) {
-    imgItem.addEventListener("click", function() {
-        bigImg.src = imgItem.src;
-    });
+smallImgs.forEach(function (imgItem) {
+  imgItem.addEventListener("click", function () {
+    bigImg.src = imgItem.src;
+  });
 });
 
 // Chọn các phần tử giới thiệu, chi tiết và bảo quản
@@ -17,33 +19,61 @@ const baoquan = document.querySelector(".baoquan");
 
 // Thêm sự kiện click cho từng nút để hiển thị nội dung tương ứng
 if (gioithieu) {
-    gioithieu.addEventListener("click", function() {
-        document.querySelector(".product-content-right-bottom-content-gioithieu").style.display = "block";
-        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "none";
-        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "none";
-    });
+  gioithieu.addEventListener("click", function () {
+    document.querySelector(
+      ".product-content-right-bottom-content-gioithieu"
+    ).style.display = "block";
+    document.querySelector(
+      ".product-content-right-bottom-content-chitiet"
+    ).style.display = "none";
+    document.querySelector(
+      ".product-content-right-bottom-content-baoquan"
+    ).style.display = "none";
+  });
 }
 
 if (chitiet) {
-    chitiet.addEventListener("click", function() {
-        document.querySelector(".product-content-right-bottom-content-gioithieu").style.display = "none";
-        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "block";
-        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "none";
-    });
+  chitiet.addEventListener("click", function () {
+    document.querySelector(
+      ".product-content-right-bottom-content-gioithieu"
+    ).style.display = "none";
+    document.querySelector(
+      ".product-content-right-bottom-content-chitiet"
+    ).style.display = "block";
+    document.querySelector(
+      ".product-content-right-bottom-content-baoquan"
+    ).style.display = "none";
+  });
 }
 
 if (baoquan) {
-    baoquan.addEventListener("click", function() {
-        document.querySelector(".product-content-right-bottom-content-gioithieu").style.display = "none";
-        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "none";
-        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "block";
-    });
+  baoquan.addEventListener("click", function () {
+    document.querySelector(
+      ".product-content-right-bottom-content-gioithieu"
+    ).style.display = "none";
+    document.querySelector(
+      ".product-content-right-bottom-content-chitiet"
+    ).style.display = "none";
+    document.querySelector(
+      ".product-content-right-bottom-content-baoquan"
+    ).style.display = "block";
+  });
 }
 
 // Chọn nút bật/tắt phần nội dung lớn
 const butTon = document.querySelector(".product-content-right-bottom-top");
 if (butTon) {
-    butTon.addEventListener("click", function() {
-        document.querySelector(".product-content-right-bottom-content-big").classList.toggle("activeB");
-    });
+  butTon.addEventListener("click", function () {
+    document
+      .querySelector(".product-content-right-bottom-content-big")
+      .classList.toggle("activeB");
+  });
 }
+document.addEventListener("DOMContentLoaded", (event) => {
+  const searchInput = document.getElementById("searchInput");
+
+  searchInput.addEventListener("change", function () {
+    console.log("Search input changed:", searchInput.value);
+    // Thực hiện các hành động khác khi nội dung của ô input thay đổi
+  });
+});
