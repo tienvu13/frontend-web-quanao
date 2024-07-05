118dc29070bba5f8c2d3fc0e39ba148d6c40bd53
bird664 <163318982+bird664@users.noreply.github.com>
2024-07-05 12:02:13 +0700

    123

---
+++ js/backtotop.js
@@ -0,0 +1,17 @@
+let mybutton = document.getElementById("backToTopBtn")
+window.onscroll = function () {
+    scrollFunction()
+};
+function scrollFunction(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction(){
+    window.scrollTo({
+        top:0,
+        behavior: 'smooth'
+    });
+};
